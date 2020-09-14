105282e36483b0c16c6c74a2c1e171601ec8a4d9
Renan Miyamoto <renanmmiyamoto@gmail.com>
2020-09-14 20:13:43 -0300

    NAC 01 - 2 SEM

--- edge/sender.py
+++ edge/sender.py
@@ -1,15 +1,19 @@
 import time
+import datetime
 from connection import Connection
 
 class Sender:
-    def __init__(self, port, baud):
+    def __init__(self, port, baud, datetime):
         self.port = port
         self.baud = baud
         self.conn = Connection(port=port, baud=baud).connect()
+        self.datetime = datetime
 
     def alimentar(self):
         if(self.conn != 0):
-            self.conn.write('open'.encode())
+            if (datetime.datetime.now() >= self.datetime):
+                self.conn.write('open'.encode())
+                self.datetime = datetime.datetime.now() + datetime.timedelta(microseconds=60)
         else:
             time.sleep(30)
             self.conn = Connection(port=self.port, baud=self.baud).connect()
